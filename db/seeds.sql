b01ed6810ccd491705d170dcabbbd5e8ae40aedf
chulopapi <marlon.guandique@gmail.com>
2020-09-17 21:36:02 -0700

    adding connection.js and DB files for schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO user (username, email, password)
+VALUES 
+  ("marlon", "marlon.guandique@gmai.com", "apple123”);
+
+INSERT INTO post (title, post_text, user_id, created_at, updated_at)
+VALUES 
+  ("test link to", "https://safr.chulopapi.com", 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO comment (comment_text, user_id, post_id, created_at, updated_at)
+VALUES 
+  ("Comments for this test!",", 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
